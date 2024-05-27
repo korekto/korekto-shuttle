c4a92ad9e8a2ab9e3ae0016ce44526c1cfcd6640
ledoyen <ledoyen.loic@gmail.com>
2024-05-27 20:39:55 +0200

    Time out old tasks

--- src/config.rs
+++ src/config.rs
@@ -1,8 +1,8 @@
 use shuttle_runtime::SecretStore;
-use std::num::{NonZeroU16, NonZeroU32, NonZeroUsize};
-use std::str::FromStr;
+use std::num::NonZeroUsize;
+use validator::Validate;
 
-#[derive(serde::Deserialize, Clone)]
+#[derive(serde::Deserialize, Clone, validator::Validate)]
 pub struct Config {
     pub base_url: String,
     #[serde(default)]
@@ -22,48 +22,47 @@ pub struct Config {
     #[serde(default)]
     pub github_runner_callback_url_override: Option<String>,
     #[serde(default = "default_scheduler_interval_in_secs")]
-    pub scheduler_interval_in_secs: NonZeroU32,
+    #[validate(range(min = 1))]
+    pub scheduler_interval_in_secs: u64,
     #[serde(default = "default_min_grading_interval_in_secs")]
-    pub min_grading_interval_in_secs: NonZeroU16,
+    #[validate(range(min = 1))]
+    pub min_grading_interval_in_secs: i32,
+    #[serde(default = "default_grading_ordered_timeout_in_secs")]
+    #[validate(range(min = 1))]
+    pub grading_ordered_timeout_in_secs: i32,
+    #[serde(default = "default_grading_started_timeout_in_secs")]
+    #[validate(range(min = 1))]
+    pub grading_started_timeout_in_secs: i32,
     #[serde(default = "default_max_parallel_gradings")]
-    pub max_parallel_gradings: NonZeroU16,
+    #[validate(range(min = 1))]
+    pub max_parallel_gradings: i32,
 }
 
 fn default_github_runner_workflow_id() -> String {
-    #[allow(clippy::expect_used)]
-    String::from_str("grade.yml").expect("Infallible !")
-}
-fn default_scheduler_interval_in_secs() -> NonZeroU32 {
-    #[allow(clippy::expect_used)]
-    NonZeroU32::new(15).expect("Infallible !")
+    String::from("grade.yml")
 }
 
-fn default_min_grading_interval_in_secs() -> NonZeroU16 {
-    #[allow(clippy::expect_used)]
-    NonZeroU16::new(15 * 60).expect("Infallible !")
+const fn default_scheduler_interval_in_secs() -> u64 {
+    15
 }
 
-fn default_max_parallel_gradings() -> NonZeroU16 {
-    #[allow(clippy::expect_used)]
-    NonZeroU16::new(3).expect("Infallible !")
+const fn default_min_grading_interval_in_secs() -> i32 {
+    20 * 60
+}
+
+const fn default_grading_ordered_timeout_in_secs() -> i32 {
+    5 * 60
+}
+
+const fn default_grading_started_timeout_in_secs() -> i32 {
+    15 * 60
+}
+
+const fn default_max_parallel_gradings() -> i32 {
+    3
 }
 
 impl Config {
-    #[must_use]
-    pub fn scheduler_interval_in_secs(&self) -> u64 {
-        u64::from(self.scheduler_interval_in_secs.get())
-    }
-
-    #[must_use]
-    pub fn min_grading_interval_in_secs(&self) -> i32 {
-        i32::from(self.min_grading_interval_in_secs.get())
-    }
-
-    #[must_use]
-    pub fn max_parallel_gradings(&self) -> i32 {
-        i32::from(self.max_parallel_gradings.get())
-    }
-
     #[must_use]
     pub fn server_base_url(&self) -> &str {
         self.server_base_url.as_deref().unwrap_or(&self.base_url)
@@ -74,6 +73,8 @@ impl TryFrom<SecretStore> for Config {
     type Error = anyhow::Error;
 
     fn try_from(value: SecretStore) -> Result<Self, Self::Error> {
-        Ok(envy::from_iter::<_, Self>(value)?)
+        let config = envy::from_iter::<_, Self>(value)?;
+        config.validate()?;
+        Ok(config)
     }
 }
---
+++ src/service/grading_tasks.rs
@@ -0,0 +1,105 @@
+use crate::github;
+use crate::repository::{grading_task::Status, Repository};
+use crate::service::Service;
+use std::fmt;
+use tracing::warn;
+
+#[derive(Default)]
+pub struct TaskStats {
+    pub ordered: i32,
+    pub errored: i32,
+    pub ordered_timeout: i32,
+    pub started_timeout: i32,
+}
+
+impl fmt::Display for TaskStats {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        write!(
+            f,
+            "Tasks: 🚀 ordered={}, ❌ errored={}, ⏱️ ordered_timeout={}, ⏱️ started_timeout={}",
+            self.ordered, self.errored, self.ordered_timeout, self.started_timeout
+        )
+    }
+}
+
+impl Service {
+    pub async fn schedule_tasks(
+        &self,
+        min_execution_interval_in_secs: i32,
+        ordered_timeout_in_secs: i32,
+        started_timeout_in_secs: i32,
+        max_tasks: i32,
+        runner: &github::runner::Runner,
+    ) -> anyhow::Result<TaskStats> {
+        let mut stats = TaskStats::default();
+
+        self.launch_grading_tasks(
+            min_execution_interval_in_secs,
+            max_tasks,
+            runner,
+            &mut stats,
+        )
+        .await?;
+
+        stats.ordered_timeout += self
+            .repo
+            .timeout_grading_tasks(&Status::ORDERED, ordered_timeout_in_secs)
+            .await?;
+        stats.started_timeout += self
+            .repo
+            .timeout_grading_tasks(&Status::STARTED, started_timeout_in_secs)
+            .await?;
+
+        Ok(stats)
+    }
+
+    async fn launch_grading_tasks(
+        &self,
+        min_execution_interval_in_secs: i32,
+        max_tasks: i32,
+        runner: &github::runner::Runner,
+        stats: &mut TaskStats,
+    ) -> anyhow::Result<()> {
+        let mut transaction = self.repo.start_transaction().await?;
+
+        let tasks = Repository::reserve_grading_tasks_to_execute_transact(
+            min_execution_interval_in_secs,
+            max_tasks,
+            &mut *transaction,
+        )
+        .await?;
+
+        for task in &tasks {
+            let send_result = runner
+                .send_grading_command(task)
+                .await
+                .map_err(|err| format!("not ordered: {err:?}"));
+
+            match send_result {
+                Ok(()) => {
+                    Repository::update_grading_task_non_terminal_status_transact(
+                        &task.uuid,
+                        &Status::ORDERED,
+                        &mut *transaction,
+                    )
+                    .await?;
+                    stats.ordered += 1;
+                }
+                Err(err) => {
+                    warn!("Grading task errored: {err}");
+                    stats.errored += 1;
+                    Repository::delete_grading_task_transact(
+                        &task.uuid,
+                        Some(err),
+                        &mut *transaction,
+                    )
+                    .await?;
+                }
+            }
+        }
+
+        transaction.commit().await?;
+
+        Ok(())
+    }
+}
